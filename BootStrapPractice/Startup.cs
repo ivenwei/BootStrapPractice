703d2c4ba211025117af7ba961f105fc12c951e5
IvanLin <ivanlin@mxic.com.tw>
2016-10-08 11:44:42 +0800

    加入專案檔案。

---
+++ BootStrapPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootStrapPractice.Startup))]
+namespace BootStrapPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

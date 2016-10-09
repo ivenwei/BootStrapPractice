8b3fa20f6f62fa22ba0dca29949665bcaf1b4623
IvanLin <ivanlin@mxic.com.tw>
2016-10-09 11:05:34 +0800

    完成首頁內容下半部分(1) 使用media

--- BootStrapPractice/Scripts/jqueryForLayout.js
+++ BootStrapPractice/Scripts/jqueryForLayout.js
@@ -11,17 +11,17 @@
     //})
     ////console.log($('.carousel-inner img').height)
 
-    //设置垂直居中
-    $('.carousel-control').css('line-height', $('.carousel-inner img').height() + 'px');
-    $(window).resize(function () {
-        //各別取得 1張圖  2張圖  3張圖 的高度  不為0的為目前的圖片  將高度給顯示的CSS
-        var $height = $('.carousel-inner img').eq(0).height() ||
-					  $('.carousel-inner img').eq(1).height() ||
-					  $('.carousel-inner img').eq(2).height();
-        $('.carousel-control').css('line-height', $height + 'px');
-    });
+    //设置垂直居中  註釋掉為不使用JQ版本
+    //$('.carousel-control').css('line-height', $('.carousel-inner img').height() + 'px');
+    //$(window).resize(function () {
+    //    //各別取得 1張圖  2張圖  3張圖 的高度  不為0的為目前的圖片  將高度給顯示的CSS
+    //    var $height = $('.carousel-inner img').eq(0).height() ||
+	//				  $('.carousel-inner img').eq(1).height() ||
+	//				  $('.carousel-inner img').eq(2).height();
+    //    $('.carousel-control').css('line-height', $height + 'px');
+    //});
 
-    //輪播圖自動撥放
+    //輪播圖自動撥放 
     $('#myCarousel').carousel({
         //4秒
         interval: 4000,

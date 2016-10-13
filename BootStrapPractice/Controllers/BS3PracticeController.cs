651f8b9e8c5f9fd688d8b7715cd4a47125481d59
IvanLin <ivanlin@mxic.com.tw>
2016-10-13 21:36:05 +0800

    排版樣式練習

---
+++ BootStrapPractice/Controllers/BS3PracticeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootStrapPractice.Controllers
+{
+    public class BS3PracticeController : Controller
+    {
+        // GET: BS3Practice
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BS3Practice
+        public ActionResult BS3PracticeLayout()
+        {
+            return View();
+        }
+
+    }
+}

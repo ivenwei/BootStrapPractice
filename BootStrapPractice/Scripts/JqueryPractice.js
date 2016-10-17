63b48e707ee9e1bae281b86fd8f8bf44929c26a7
IvanLin <ivanlin@mxic.com.tw>
2016-10-17 21:28:32 +0800

    Jquery練習1 - 基礎核心練習

---
+++ BootStrapPractice/Scripts/JqueryPractice.js
@@ -0,0 +1,31 @@
+﻿//$(function () {
+
+//    $('#box').css('color', 'red').css('font-size','200px').css('font-weight');
+//});
+
+//$(document).ready(function () {
+//    alert(1)
+//})
+
+//$(document).ready(function () {
+//    alert(2)
+//})
+
+//$(function () {
+//    alert(1)
+//})
+//$(function () {
+//    alert(2)
+//})
+
+$(function () {
+    //alert($);			//jQuery对象的内部
+    //alert($());			//返回的jQuery对象
+    //alert($('#box'));	//返回的也是jQuery对象
+    //alert($('#box').css('color', 'red'));		//还是返回的jQuery对象
+    //alert(document.getElementById('box'));  //[object HTMLDivElement]，原生DOM对象
+
+    //alert($('#box').get(0));  //取回ID回BOX的第一筆DOM对象
+
+    //alert($(document.getElementById('box')).css('color', 'red'));		//jQuery对象和DOM对象之间的互换
+})

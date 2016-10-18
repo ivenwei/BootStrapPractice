787d3f8743be12099ad161fbde779bff33647c68
IvanLin <ivanlin@mxic.com.tw>
2016-10-18 21:30:46 +0800

    完成練習輔助類和響應式工具內容

--- BootStrapPractice/Controllers/BS3PracticeController.cs
+++ BootStrapPractice/Controllers/BS3PracticeController.cs
@@ -38,5 +38,11 @@ namespace BootStrapPractice.Controllers
         {
             return View();
         }
+
+        // GET: BS3Practice
+        public ActionResult BS3PracticeRWDTools()
+        {
+            return View();
+        }
     }
 }
